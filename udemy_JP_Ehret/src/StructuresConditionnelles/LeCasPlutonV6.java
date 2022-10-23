ea09bb68441a17fb9e43b84654b9544f84afdae7
Da costa Philippe <Dacosta-philippe@laposte.net>
2022-10-23 09:31:15 +0200

    ajout des derniers travaux

--- udemy_JP_Ehret/src/StructuresConditionnelles/LeCasPlutonV6.java
+++ udemy_JP_Ehret/src/StructuresConditionnelles/LeCasPlutonV6.java
@@ -5,7 +5,7 @@ public class LeCasPlutonV6 {
 	public static void main(String[] args) {
 		 String phraseIntroduction="En %d, les planètes du système solaire étaient au nombre de : %d";
 	        int nbPlanetes=0;
-	        short annee=2012;
+	        short annee=2005;
 	        if (annee<2006){
 	            nbPlanetes=9;
 	        }else
